03468f465a690a8241aba19b7c1acc1902735f56
Emil Rosenberg <emil.rosenberg@acorntechnology.se>
2024-09-18 13:16:15 +0200

    remove slint stuff

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-include!(env!("SLINT_INCLUDE_GENERATED"));
-
 use clap::Parser;
 use core::fmt;
 use std::error::Error;
@@ -118,17 +113,5 @@ fn main() -> Result<(), Box<dyn Error>> {
     // // 1. Front end TUI
     // // 2. clean up
     // //  */
-    // // let ui = AppWindow::new()?;
-
-    // // // ui.on_request_increase_value({
-    // // //     let ui_handle = ui.as_weak();
-    // // //     move || {
-    // // //         let ui = ui_handle.unwrap();
-    // // //         ui.set_counter(ui.get_counter() + 1);
-    // // //     }
-    // // // });
-
-    // // ui.run()?;
-
     Ok(())
 }
