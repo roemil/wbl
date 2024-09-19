5aaa9f4a436ce943669dcc03ce15986615b587ff
Emil Rosenberg <emil.rosenberg@acorntechnology.se>
2024-09-19 15:57:23 +0200

    remove dead code

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,9 @@ use serde::{Deserialize, Serialize};
 pub mod calc_wb;
 pub mod planes;
 
-#[derive(Default, PartialEq, Eq, Hash, Debug, Clone, Copy, PartialOrd, Ord,Deserialize, Serialize)]
+#[derive(
+    Default, PartialEq, Eq, Hash, Debug, Clone, Copy, PartialOrd, Ord, Deserialize, Serialize,
+)]
 pub enum Kind {
     #[default]
     NoValue,
@@ -20,11 +22,11 @@ pub enum Kind {
     CoPilot,
     PaxLeftBack,
     PaxRightBack,
-    TripFuel
+    TripFuel,
 }
 
 #[derive(Debug)]
-pub enum FailReason{
+pub enum FailReason {
     Bagage,
     BagageFront,
     BagageBack,
@@ -34,7 +36,7 @@ pub enum FailReason{
     Fuel,
     ZeroFuel,
     LandingFuel,
-    TorqueOutOfBounds
+    TorqueOutOfBounds,
 }
 
 impl FromStr for Kind {
@@ -54,7 +56,7 @@ impl FromStr for Kind {
             "passenger_left" => Ok(Kind::PaxLeftBack),
             "passenger_right" => Ok(Kind::PaxRightBack),
             "trip_fuel" => Ok(Kind::TripFuel),
-            _ => Err(format!("Invalid value of string: {}", s))
+            _ => Err(format!("Invalid value of string: {}", s)),
         }
     }
 }
@@ -71,7 +73,7 @@ fn is_value_within_weight_limit(
     is_item_within_limit
 }
 
-#[derive(PartialEq, PartialOrd, Debug, Clone,Deserialize, Serialize)]
+#[derive(PartialEq, PartialOrd, Debug, Clone, Deserialize, Serialize)]
 pub struct WeightLever {
     pub weight: f32,
     pub lever: f32,
@@ -94,7 +96,11 @@ impl WeightLever {
 }
 
 //ref: https://www.linkedin.com/pulse/short-formula-check-given-point-lies-inside-outside-polygon-ziemecki/
-pub fn is_inside_polygon(point: WeightLever, vertices: &[WeightLever; 6], valid_border: bool) -> Result<(), FailReason> {
+pub fn is_inside_polygon(
+    point: WeightLever,
+    vertices: &[WeightLever; 6],
+    valid_border: bool,
+) -> Result<(), FailReason> {
     let mut sum = num::complex::Complex::new(0.0, 0.0);
 
     for i in 1..vertices.len() + 1 {
@@ -102,7 +108,11 @@ pub fn is_inside_polygon(point: WeightLever, vertices: &[WeightLever; 6], valid_
         let v1 = &vertices[i % vertices.len()];
 
         if is_point_in_segment(&point, v0, v1) {
-            if valid_border {return Ok(())} else {return Err(FailReason::TorqueOutOfBounds)}
+            if valid_border {
+                return Ok(());
+            } else {
+                return Err(FailReason::TorqueOutOfBounds);
+            }
         }
         let v1_c = num::complex::Complex::new(v1.lever, v1.weight);
         let p_c = num::complex::Complex::new(point.lever, point.weight);
@@ -128,40 +138,11 @@ fn is_point_in_segment(p: &WeightLever, p0: &WeightLever, p1: &WeightLever) -> b
         || (p1.weight == 0.0 && p1.lever == 0.0)
 }
 
-
-
-#[derive(Deserialize, Serialize)]
-pub struct MoaConfig {
-    pub config: std::collections::HashMap<Kind, f32>,
-    pub vertices: [WeightLever; 6],
-}
-
-
-
-impl Default for MoaConfig {
-    fn default() -> MoaConfig {
-        MoaConfig {
-            config: HashMap::new(),
-            vertices: [
-                WeightLever::new(490.0, 171.2),
-                WeightLever::new(600.0, 171.2),
-                WeightLever::new(750.0, 179.2),
-                WeightLever::new(750.0, 184.0),
-                WeightLever::new(600.0, 184.0),
-                WeightLever::new(490.0, 184.0),
-            ],
-        }
-    }
-}
-
-
-impl MoaConfig {
-    pub fn new() -> MoaConfig {
-        MoaConfig::default()
-    }
-}
-
-pub fn update_weight(airplane_properties : &mut HashMap<Kind, WeightLever>, kind : Kind, weight : f32) -> Result<(), String>{
+pub fn update_weight(
+    airplane_properties: &mut HashMap<Kind, WeightLever>,
+    kind: Kind,
+    weight: f32,
+) -> Result<(), String> {
     if let Some(arm) = airplane_properties.get_mut(&kind) {
         arm.weight = weight;
         return Ok(());
